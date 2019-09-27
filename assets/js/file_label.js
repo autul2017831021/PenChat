9bb84cba859a2ae14cad30ab8286008a62fb70a5
ImrulKay3s <kayesimrul224@gmail.com>
2019-09-27 09:38:35 -0800

    Design The signup area

--- assets/js/file_label.js
+++
@@ -1,6 +0,0 @@
- $(document).ready(function(){
- 	$(document).on("change","#file",function(){
- 		var image_name = $("#file").val();
- 		$("#file-label").html(image_name);
- 	})
- })
---
+++ components/css.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="assets/fontawesome/css/all.min.css">
+<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,900&display=swap" rel="stylesheet">
+<link rel="icon" type="image" href="assets/img/favicon.png">
