8ad5fee38ae3d19e96297434444a58eec50bcc9e
ImrulKay3s <kayesimrul224@gmail.com>
2019-09-27 09:42:14 -0800

    Create the signup form

---
+++ components/signup.php
@@ -0,0 +1,26 @@
+<div class="form-area">
+	<form method="POST" action="">
+		<div class="group">
+			<h2 class="form-heading">Create New Account</h2>
+		</div>
+		<div class="group">
+			<input type="text" name="full_name" class="control" placeholder="Enter Full Name">
+		</div>
+		<div class="group">
+			<input type="email" name="email" class="control" placeholder="Enter Email Address">
+		</div>
+		<div class="group">
+			<input type="password" name="password" class="control" placeholder="Create Password">
+		</div>
+		<div class="group">
+			<label for="file" id="file-label"><i class="fas fa-cloud-upload-alt upload-icon"></i>Choose image</label>
+			<input type="file" name="img" class="file" id="file">
+		</div>
+		<div class="group">
+			<input type="submit" name="signup" class="btn account-btn" value="Create Account">
+		</div>
+		<div class="group">
+			<a href="login.php" class="link">Already have an account</a>
+		</div>
+	</form>
+</div>
