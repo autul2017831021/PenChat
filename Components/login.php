691f5d98c7bf65db757338f7e014c727f419c481
autul2017831021 <43562788+autul2017831021@users.noreply.github.com>
2019-09-28 23:26:03 +0600

    Add files via upload
    login has benn added

---
+++ Components/login.php
@@ -0,0 +1,21 @@
+<div class="form-area">
+	<form method="POST" action="">
+		<div class="group">
+			<h2 class="form-heading">User Login</h2>
+		</div>
+		
+		<div class="group">
+			<input type="email" name="email" class="control" placeholder="Enter Email Address">
+		</div>
+		<div class="group">
+			<input type="password" name="password" class="control" placeholder="Enter Password">
+		</div>
+		
+		<div class="group">
+			<input type="submit" name="login" class="btn account-btn" value="User Login">
+		</div>
+		<div class="group">
+			<a href="signup.php" class="link">Create New Account</a>
+		</div>
+	</form>
+</div>
