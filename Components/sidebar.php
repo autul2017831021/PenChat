f1ff043c60595209002703393de60a3cbd43c513
autul2017831021 <43562788+autul2017831021@users.noreply.github.com>
2019-09-29 04:29:43 +0600

    Update sidebar.php

--- Components/sidebar.php
+++ Components/sidebar.php
@@ -1,7 +1,7 @@
 <section id="sidebar">
 		<ul class="left-ul">
-			<li><a href="#"><span class="profile-img-span"><img src="assets/img/pro4.jpg" alt="Profile image" class="profile-img"></span></a></li>
-			<li><a href="index.php">Shakil Khan <span class="cool-hover"></span></a></li>
+			<li><a href="#"><span class="profile-img-span"><img src="assets/img/autul.jpg" alt="Profile image" class="profile-img"></span></a></li>
+			<li><a href="index.php">Autul Rahman <span class="cool-hover"></span></a></li>
 			<li><a href="change_name.php">Change Name <span class="cool-hover"></span></a></li>
 			<li><a href="change_password.php">Change Password <span class="cool-hover"></span></a></li>
 			<li><a href="#">110 User Online <span class="cool-hover"></span></a></li>
