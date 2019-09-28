ffa18616981420b8bae6eec83e9d390a0b4cda74
autul2017831021 <43562788+autul2017831021@users.noreply.github.com>
2019-09-29 00:35:26 +0600

    Add files via upload

---
+++ Components/messages.php
@@ -0,0 +1,112 @@
+<div class="messages">
+				<div class="left-message common-margin">
+					<div class="sender-img-block">
+						<img src="assets/img/autul.jpg" class="sender-img">
+						<span class="online-icon">
+							
+						</span>
+					</div>
+					<div class="left-msg-area">
+						<div class="user-name-date">
+							<span class="sender-name">
+								Autul Rahman
+							</span>
+							<span class="date-time">
+								1 day ago
+							</span>
+						</div>
+						<div class="left-msg">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur ea placeat ab non quasi nisi obcaecati ipsa, rem animi voluptatum dicta! Porro, voluptate. Minima quam sit aliquid quod, velit quae.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="left-message common-margin">
+					<div class="sender-img-block">
+						<img src="assets/img/autul.jpg" class="sender-img">
+						<span class="offline-icon">
+							
+						</span>
+					</div>
+					<div class="left-msg-area">
+						<div class="user-name-date">
+							<span class="sender-name">
+								Autul Rahman
+							</span>
+							<span class="date-time">
+								1 day ago
+							</span>
+						</div>
+						<div class="left-msg">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur ea placeat ab non quasi nisi obcaecati ipsa, rem animi voluptatum dicta! Porro, voluptate. Minima quam sit aliquid quod, velit quae.
+							</p>
+						</div>
+					</div>
+				</div>
+				
+				<div class="right-messages common-margin">
+					<div class="right-msg-area">
+						<span class="date-time right-time">
+								1 day ago
+						</span>
+						<div class="right-msg">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores impedit odio quis temporibus hic. Laborum deleniti modi, veniam dignissimos alias delectus, libero natus porro fuga voluptatibus ullam consequuntur, pariatur ab.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="right-messages common-margin">
+					<div class="right-msg-area">
+						<span class="date-time right-time">
+								1 day ago
+						</span>
+						<div class="right-msg">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores impedit odio quis temporibus hic. Laborum deleniti modi, veniam dignissimos alias delectus, libero natus porro fuga voluptatibus ullam consequuntur, pariatur ab.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="right-messages common-margin">
+					<div class="right-msg-area">
+						<span class="date-time right-time">
+								1 day ago
+						</span>
+						<div class="right-msg">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores impedit odio quis temporibus hic. Laborum deleniti modi, veniam dignissimos alias delectus, libero natus porro fuga voluptatibus ullam consequuntur, pariatur ab.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="right-messages common-margin">
+					<div class="right-msg-area">
+						<span class="date-time right-time">
+								1 day ago
+						</span>
+						<div class="right-msg">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores impedit odio quis temporibus hic. Laborum deleniti modi, veniam dignissimos alias delectus, libero natus porro fuga voluptatibus ullam consequuntur, pariatur ab.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="right-messages common-margin">
+					<div class="right-msg-area">
+						<span class="date-time right-time">
+								1 day ago
+						</span>
+						<div class="right-msg">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores impedit odio quis temporibus hic. Laborum deleniti modi, veniam dignissimos alias delectus, libero natus porro fuga voluptatibus ullam consequuntur, pariatur ab.
+							</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
