3f388c852e53be57522f8575116af3ccef49a3cb
autul2017831021 <43562788+autul2017831021@users.noreply.github.com>
2019-09-28 23:23:53 +0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,intial-scale=1,shrink-to-fit=no">
+	<title>Create New Account</title>
+	<?php include 'component/css.php'; ?>
+</head>
+<body>
+	<div class="signup-container">
+		<div class="account-left">
+			<div class="account-text">
+				<h1>Lets Chat</h1>
+				<p>Band is an up-and-coming chat app for groups. You can create all the groups you want to and invite all the people you want to them. The developers recommend this app to people on sports teams, school groups, gaming clans, work groups, and anything like that. Everybody joins in, chats it up, and has a good time</p>
+			</div>
+		</div>
+		<div class="account-right">
+			<?php include 'components/login.php'; ?>
+		</div>
+	</div>
+	<?php include 'components/js.php' ?>
+	<!-- <script type="text/javascript" src="assets/js/file_label.js"> </script> -->
+</body>
+</html>
